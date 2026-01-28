3e20874fbd06dfd37c0ed2a7d3889da8d54c004f
JeremyDevCode <jeremydevcode@gmail.com>
2026-01-27 22:04:36 -0500

    feat(config): add DatabaseURL for Postgres

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,12 +3,14 @@ package config
 import "os"
 
 type Config struct {
-	Port string
+	Port         string
+	DatabaseURL  string
 }
 
 func Load() *Config {
 	return &Config{
-		Port: getEnv("PORT", "8080"),
+		Port:        getEnv("PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
 	}
 }
 
