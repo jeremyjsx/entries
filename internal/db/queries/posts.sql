9d9c332c58356abfa5f7687606f823fc8d5dbcc2
JeremyDevCode <jeremydevcode@gmail.com>
2026-01-28 21:51:30 -0500

    feat: add query to get post by slug

--- internal/db/queries/posts.sql
+++ internal/db/queries/posts.sql
@@ -2,3 +2,6 @@
 INSERT INTO posts (title, slug, s3_key, status)
 VALUES ($1, $2, $3, $4)
 RETURNING id, title, slug, s3_key, status, created_at, updated_at;
+
+-- name: GetPostBySlug :one
+SELECT id, title, slug, s3_key, status, created_at, updated_at FROM posts WHERE slug = $1;
